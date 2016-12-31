da8b52888f5523aeb15e950472ff5a8876626375
Goutham S <gouthamsajeev1996@gmail.com>
2016-12-31 19:32:41 +0530

    Update MinMax.java

--- Basic Programming/Implementation/MinMax.java
+++ Basic Programming/Implementation/MinMax.java
@@ -3,14 +3,14 @@ import java.io.*;
 class MinMax{
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-        int length = in.nextInt();
-        Set<Integer> set = new HashSet<Integer>();
-        for(int i=0;i<length;i++){
-            set.add(in.nextInt());
-        }
-        Object[] arr = set.toArray();
-        Arrays.sort(arr);
-        System.out.println((set.size()-1 == ((int)arr[arr.length-1] -(int) arr[0]))?"YES":"NO");
+		int length = in.nextInt();
+		Set<Integer> set = new HashSet<Integer>();
+		for(int i=0;i<length;i++){
+		    set.add(in.nextInt());
+		}
+		Object[] arr = set.toArray();
+		Arrays.sort(arr);
+		System.out.println((set.size()-1 == ((int)arr[arr.length-1] -(int) arr[0]))?"YES":"NO");
 	}
 
 }
