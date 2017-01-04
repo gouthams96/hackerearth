5de3ae4cd571fde9fead3c9fbb45213bf8a40d2e
Goutham S <gouthamsajeev1996@gmail.com>
2017-01-04 12:17:06 +0530

    Update ToggleString.java

--- Basic Programming/Input Output/ToggleString.java
+++ Basic Programming/Input Output/ToggleString.java
@@ -1,36 +1,12 @@
-
-import java.util.Scanner;
-
-/**
- *
- * @author Goutham S
- */
-public class ToggleString {
-       
-   
-    public static void main(String[] args) {
-        String orginalString = new String();
-        Scanner in = new Scanner(System.in);
-        orginalString = in.nextLine();
-          for (int i=0;i<orginalString.length();i++ ) {
-                if(orginalString.charAt(i) <= 90 && orginalString.charAt(i) >= 65){
-
-                                    String  letter = Character.toString(orginalString.charAt(i));
-                                    System.out.print(letter.toLowerCase());
-                }
-                else if(orginalString.charAt(i) <= 122 && orginalString.charAt(i) >= 97){
-                                    String  letter = Character.toString(orginalString.charAt(i));
-                                    System.out.print(letter.toUpperCase());
-                }else{
-                        System.out.print(orginalString.charAt(i));
-                }      
-            }
-            
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+class TestClass {
+    public static void main(String args[] ) throws Exception {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        for(char c: s.toCharArray()){
+            System.out.print(Character.isLowerCase(c)?Character.toUpperCase(c):Character.toLowerCase(c));
         }
-
-    
+     
+    }
 }
-        
-
-    
-
